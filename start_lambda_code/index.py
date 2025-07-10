787b6184a2c0895ab7a0f8ea0dd262248cdd2d52
George904 <88448334+George904@users.noreply.github.com>
2025-07-10 16:55:05 -0500

    Delete start_lambda_code directory

--- start_lambda_code/index.py
+++
@@ -1,8 +0,0 @@
-import boto3  
-region = 'us-east-2'
-instances = ['i-0bc81ad16df8d41e7']
-ec2 = boto3.client('ec2', region_name=region)
-
-def lambda_handler(event, context):
-    ec2.start_instances(InstanceIds=instances)
-    print('started your instances: ' + str(instances))
